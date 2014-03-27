466d5da1073c7f329c0e95330e22facb5875dce3
roohy <shemirani.r@gmail.com>
2014-03-28 03:41:35 +0430

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	
+	var json = $('#entry-container textarea').eq(0).val('');
+	
+	$('#submit-button').click(function(){
+		var json = $('#entry-container textarea').eq(0).val();
+		console.log('json object: '+json);
+		json = $.parseJSON(json);
+		$('#input-container').slideUp(2000,function(){
+			$('#json-parsed').slideDown(2000);
+		});
+		$('#jsonview').jsonView(json);
+	});
+});
